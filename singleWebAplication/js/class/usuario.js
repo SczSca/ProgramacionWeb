dad54862afd828dfec7c25cdacd9efeafff8c8a1
SczSca <aalvarez59@ucol.mx>
2023-03-08 09:38:51 -0600

    Eliminación de metodos no utilizados

--- singleWebAplication/js/class/usuario.js
+++ singleWebAplication/js/class/usuario.js
@@ -22,35 +22,9 @@ class Usuarios {
         return userPosicion;
     }
     listar(){
-        return this.data
+        return this.data;
     }
-    rankForBSearch(key){
-        let lo = 0;
-        let hi = this.data.length - 1;
-        let mid = 0;
-        while(lo <= hi){
-            mid = Math.floor(lo + (hi - lo) / 2);
-            if(this.data[mid]['id'] > key){
-                hi = mid - 1;
-            }
-            else if(this.data[mid]['id'] < key){
-                lo = mid + 1;
-            }
-            else {
-                return [1,mid];
-            }
-        }
-        return [null,mid];
-        //checar si funciona retornando un solo valor if(found) return mid; elseif(notfound) return null;
-    }
-    binarySearch(code){
-        let posicion = this.rankForBSearch(code);
-        if(posicion[0] == null)    return posicion[0];
-        else                    return this.data[posicion[1]];
-    }
-    getUserNameFrom(posicion){
-        return this.data[posicion]['username'];
-    }
+
 }
 
 
