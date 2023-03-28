ef1b64e02725f68271b67d3763c9e48a01eb853a
SczSca <aalvarez59@ucol.mx>
2023-03-28 15:05:52 -0600

    Se agregaron comentarios para la descripcion de las operaciones y funciones

--- dbPractice/app.js
+++ dbPractice/app.js
@@ -4,42 +4,47 @@ connection.connect();
 let cliente={nombre:'Nepomuceno', apellido:'Gutierrez', rfc:'NEPO231010',ciudad:'Colima', CP:'28010', correo:'conocido@gmail.com'};
 let factura={fecha:'2023/03/23', total:150, productos:[{fkProducto:1,cantidadOrdenada:5,precioIndividual:10},{fkProducto:2,cantidadOrdenada:5,precioIndividual:20}]};
 
-connection.beginTransaction(function(err) {
+connection.beginTransaction(function(err) { //uso de transaction para probar sin insertar datos al servidor
     if (err) { throw err; }
     
+    //query para insertar al cliente de prueba
     let queryInsertCliente = connection.query('INSERT INTO clientes SET ?', cliente, function (error, results, fields) {
-        if (error){
+        if (error){ //si hay error, se cancela la transaccion
             return connection.rollback(function(){
                 throw error;    
             });
         }
+            //query que obtiene el id del cliente con el rfc del cliente en sesión
             let querySelect = connection.query('SELECT * FROM clientes WHERE RFC = ?',cliente.rfc, function (error, results, fields) {
             if (error){
                 return connection.rollback(()=>{
                     throw error;    
                 });
             } 
-            const idCliente = results[0].idCliente;
-            const datosFactura = {
+            const idCliente = results[0].idCliente; //obtiene y guarda la id del cliente en sesión
+            const datosFactura = { //json con los datos a ingresar para la factura
                 fecha:factura.fecha,
                 total:factura.total,
                 fkCLiente: idCliente
             };
+            //query para insertar la factura
             let queryInsertFactura = connection.query('INSERT INTO pedidos SET ?',datosFactura,function(error,facturaInsertada,fields){
                 if(error){
                     return connection.rollback(()=>{
                         throw error;
                     });
                 }
-                console.log(facturaInsertada);
-                for(let i = 0; i < factura.productos.length; i++){
+                console.log(facturaInsertada); //se renombro el parametro results a facturaInsertada para mayor claridad
+                
+                for(let i = 0; i < factura.productos.length; i++){//se cicla todos los productos que se van a agregar a la factura
                     const detallesFactura = {
                         fkPedido: facturaInsertada.insertId, 
                         fkProducto: factura.productos[i].fkProducto, 
                         cantidadOrdenada: factura.productos[i].cantidadOrdenada, 
                         precioIndividual: factura.productos[i].precioIndividual,
                         numeroLineaPedido: i+1 
-                    };    
+                    };
+                    //query por cada producto que solicita en una misma factura para agregar en sus detalles
                     let queryInsertDetalleFacturas = connection.query('INSERT INTO detallePedidos SET ?',detallesFactura,function(error, results, fields){
                         if(error){
                             return connection.rollback(()=>{
@@ -49,6 +54,7 @@ connection.beginTransaction(function(err) {
                         console.log(results);
                     })
                 }
+                //query para leer los detalles de la factura que se acaba de crear
                 connection.query('SELECT * FROM detallePedidos WHERE fkPedido = ?',facturaInsertada.insertId,function(error,results,fields){
                     if(error){
                         return connection.rollback(()=>{
@@ -57,9 +63,10 @@ connection.beginTransaction(function(err) {
                     }
                     console.log(results);
                 });
+                //rollback forzado para no mandar cambios al servidor
                 connection.rollback(()=>{
                     console.log("Se cancelo la transferencia");
-                    connection.end();
+                    connection.end();//se termina la conexion luego de cancelar transferencia
                 })
             });
         });
