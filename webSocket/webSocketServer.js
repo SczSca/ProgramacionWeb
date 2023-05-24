a1194dfc582148baf231c30ce9cfb3599666bf52
SczSca <aalvarez59@ucol.mx>
2023-05-24 09:16:55 -0600

    reducción y agregado de comentarios en el metodo cuando recibe mensaje

--- webSocket/webSocketServer.js
+++ webSocket/webSocketServer.js
@@ -11,13 +11,21 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     // Enviar el mensaje a todos los clientes conectados
+    /**
+     * jsonMessage {
+     * Buffer: valor,
+     * data: [arr de bytes]
+     * }
+     */
     const jsonMessage = JSON.stringify(message)
+    /**se obtiene la data del obj json message, se convierte de bytes a string y ese string se convierte a json
+     * jsonData {
+     * type: string,
+     * name: string,
+     * data: string [solo para el type === "message"]
+     */
     const jsonData = JSON.parse(String.fromCharCode(...JSON.parse(jsonMessage).data))
-    if (jsonData.type === 'login'){
-        broadcast(JSON.stringify(jsonData));
-    }else if(jsonData.type === 'message'){
-        broadcast(JSON.stringify(jsonData));
-    }else if(jsonData.type === 'exit'){
+    if (jsonData.type === 'message' || jsonData.type === 'login' || jsonData.type === 'exit'){
         broadcast(JSON.stringify(jsonData));
     }else{
         console.error(`Error: Invalid type "${jsonData.type}"`);
